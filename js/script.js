54f594de52e00c2711909e00a4b131a2b0b093f3
alexolivet <alexolivet@yahoo.com>
2016-12-19 16:46:44 +0100

    refactoring css and fixing a js save issue

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
-
 //variable to build url to rijksmuseum
-var baseURL ="https://www.rijksmuseum.nl/api/en/collection/"
+var baseURL = "https://www.rijksmuseum.nl/api/en/collection/"
 var apiKey = "?key=r4nzV2tL&imgonly=true&format=json&ps=1"
-//this is the search button on htnl page
+    //this is the search button on htnl page
 var searchBtn = document.getElementById("search");
 //add event on button
 searchBtn.addEventListener("click", doSearch);
@@ -12,9 +11,9 @@ var resultDiv = document.getElementById("result");
 var searchField = document.getElementById("query");
 // search the collection using a JSON call
 function search(query) {
-  return $.getJSON(baseURL + apiKey + "&q=Q".replace("Q", query));
-}
-//search function
+        return $.getJSON(baseURL + apiKey + "&q=Q".replace("Q", query));
+    }
+    //search function
 function doSearch() {
     //The innerHTML property sets or returns the HTML content (inner HTML) of an element.
     //in this case it is empty on each new search
@@ -22,145 +21,150 @@ function doSearch() {
     //variable for the search string
     var searchString = searchField.value;
     if (!searchString) {
-      resultDiv.innerHTML = "<p>Please submit at least a letter</p>";
+        resultDiv.innerHTML = "<p>Please submit at least a letter</p>";
     } else {
         //invoke search function
-        search(searchString).done(function(data) {//.done is similar to the success callback 
-            for (let artObj in data.artObjects) {//Loops are handy, if you want to run the same code over and over again, each time with a different value.
+        search(searchString).done(function(data) { //.done is similar to the success callback 
+            for (let artObj in data.artObjects) { //Loops are handy, if you want to run the same code over and over again, each time with a different value.
                 //The let statement declares a block scope local variable, optionally initializing it to a value.
                 var rImg = document.createElement("img");
                 rImg.setAttribute("id", "drawing");
                 rImg.setAttribute("width", "400");
                 rImg.setAttribute("height", "400");
-                rImg.setAttribute("crossOrigin","Anonymous");
+                rImg.setAttribute("crossOrigin", "Anonymous");
                 setTimeout(function() { //timeout for image load to canvas - start
-                  var c = document.getElementById("myCanvas"); //declare the canvas
-                  var ctx = c.getContext("2d");//this is needed for canvas
-                  var img = document.getElementById("drawing");//this is the source image
-                  ctx.drawImage(img, 10, 10, 379, 500);//border and canvas size
-                }, 1000);//timeout for image load to canvas - ends
-                rImg.src = data.artObjects[artObj].webImage.url;//rijksmuseum image source
+                    var c = document.getElementById("myCanvas"); //declare the canvas
+                    var ctx = c.getContext("2d"); //this is needed for canvas
+                    var img = document.getElementById("drawing"); //this is the source image
+                    ctx.drawImage(img, 0, 0, 640, 480); //border and canvas size
+                }, 1000); //timeout for image load to canvas - ends
+                rImg.src = data.artObjects[artObj].webImage.url; //rijksmuseum image source
                 //resultDiv.appendChild(rImg);
                 document.getElementById('drawing1').appendChild(rImg);
                 resultDiv.innerHTML += data.artObjects[artObj].title;
                 //resultDiv.innerHTML += "<br>&nbsp;<br>&nbsp;<br>&nbsp;<br>";
-              }
-            });
-};
+            }
+        });
+    };
 }
-//canvas drawing
-//please see url for deatils: http://stackoverflow.com/questions/2368784/draw-on-html5-canvas-using-a-mouse
-    var canvas, ctx, flag = false,
-        prevX = 0,
-        currX = 0,
-        prevY = 0,
-        currY = 0,
-        dot_flag = false;
 
-    var x = "black",
-        y = 2;
-    
-    function init() {
-        canvas = document.getElementById('myCanvas');
-        ctx = canvas.getContext("2d");
-        w = canvas.width;
-        h = canvas.height;
-    
-        canvas.addEventListener("mousemove", function (e) {
-            findxy('move', e)
-        }, false);
-        canvas.addEventListener("mousedown", function (e) {
-            findxy('down', e)
-        }, false);
-        canvas.addEventListener("mouseup", function (e) {
-            findxy('up', e)
-        }, false);
-        canvas.addEventListener("mouseout", function (e) {
-            findxy('out', e)
-        }, false);
+//reload page to clear results
+function reLoad() {
+        location.reload();
     }
-    
-    function color(obj) {
-        switch (obj.id) {
-            case "green":
-                x = "green";
-                break;
-            case "blue":
-                x = "blue";
-                break;
-            case "red":
-                x = "red";
-                break;
-            case "yellow":
-                x = "yellow";
-                break;
-            case "orange":
-                x = "orange";
-                break;
-            case "black":
-                x = "black";
-                break;
-            case "white":
-                x = "white";
-                break;
+    //canvas drawing
+    //please see url for deatils: http://stackoverflow.com/questions/2368784/draw-on-html5-canvas-using-a-mouse
+var canvas, ctx, flag = false,
+    prevX = 0,
+    currX = 0,
+    prevY = 0,
+    currY = 0,
+    dot_flag = false;
+
+var x = "black",
+    y = 2;
+
+function init() {
+    canvas = document.getElementById('myCanvas');
+    ctx = canvas.getContext("2d");
+    w = canvas.width;
+    h = canvas.height;
+
+    canvas.addEventListener("mousemove", function(e) {
+        findxy('move', e)
+    }, false);
+    canvas.addEventListener("mousedown", function(e) {
+        findxy('down', e)
+    }, false);
+    canvas.addEventListener("mouseup", function(e) {
+        findxy('up', e)
+    }, false);
+    canvas.addEventListener("mouseout", function(e) {
+        findxy('out', e)
+    }, false);
+}
+
+function color(obj) {
+    switch (obj.id) {
+        case "green":
+            x = "green";
+            break;
+        case "blue":
+            x = "blue";
+            break;
+        case "red":
+            x = "red";
+            break;
+        case "yellow":
+            x = "yellow";
+            break;
+        case "orange":
+            x = "orange";
+            break;
+        case "black":
+            x = "black";
+            break;
+        case "white":
+            x = "white";
+            break;
+    }
+    if (x == "white") y = 14;
+    else y = 2;
+
+}
+
+function draw() {
+    ctx.beginPath();
+    ctx.moveTo(prevX, prevY);
+    ctx.lineTo(currX, currY);
+    ctx.strokeStyle = x;
+    ctx.lineWidth = y;
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function erase() {
+    var m = confirm("Want to clear");
+    if (m) {
+        ctx.clearRect(0, 0, w, h);
+        document.getElementById("drawing1").style.display = "none";
+    }
+}
+
+function save() {
+    document.getElementById("canvasimg").style.border = "2px solid";
+    var dataURL = canvas.toDataURL();
+    document.getElementById("canvasimg").src = dataURL;
+    document.getElementById("canvasimg").style.display = "inline";
+}
+
+function findxy(res, e) {
+    if (res == 'down') {
+        prevX = currX;
+        prevY = currY;
+        currX = e.clientX - canvas.offsetLeft;
+        currY = e.clientY - canvas.offsetTop;
+
+        flag = true;
+        dot_flag = true;
+        if (dot_flag) {
+            ctx.beginPath();
+            ctx.fillStyle = x;
+            ctx.fillRect(currX, currY, 2, 2);
+            ctx.closePath();
+            dot_flag = false;
         }
-        if (x == "white") y = 14;
-        else y = 2;
-    
     }
-    
-    function draw() {
-        ctx.beginPath();
-        ctx.moveTo(prevX, prevY);
-        ctx.lineTo(currX, currY);
-        ctx.strokeStyle = x;
-        ctx.lineWidth = y;
-        ctx.stroke();
-        ctx.closePath();
+    if (res == 'up' || res == "out") {
+        flag = false;
     }
-    
-    function erase() {
-        var m = confirm("Want to clear");
-        if (m) {
-            ctx.clearRect(0, 0, w, h);
-            document.getElementById("drawing1").style.display = "none";
-        }
-    }
-    
-    function save() {
-        document.getElementById("drawing1").style.border = "2px solid";
-        var dataURL = canvas.toDataURL();
-        document.getElementById("drawing1").src = dataURL;
-        document.getElementById("drawing1").style.display = "inline";
-    }
-    
-    function findxy(res, e) {
-        if (res == 'down') {
+    if (res == 'move') {
+        if (flag) {
             prevX = currX;
             prevY = currY;
             currX = e.clientX - canvas.offsetLeft;
             currY = e.clientY - canvas.offsetTop;
-    
-            flag = true;
-            dot_flag = true;
-            if (dot_flag) {
-                ctx.beginPath();
-                ctx.fillStyle = x;
-                ctx.fillRect(currX, currY, 2, 2);
-                ctx.closePath();
-                dot_flag = false;
-            }
-        }
-        if (res == 'up' || res == "out") {
-            flag = false;
-        }
-        if (res == 'move') {
-            if (flag) {
-                prevX = currX;
-                prevY = currY;
-                currX = e.clientX - canvas.offsetLeft;
-                currY = e.clientY - canvas.offsetTop;
-                draw();
-            }
+            draw();
         }
     }
+}
