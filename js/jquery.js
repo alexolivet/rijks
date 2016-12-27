1cda4d5257bf6d3db55a0a9581f986b017dfd10d
alexolivet <alexolivet@yahoo.com>
2016-12-27 15:50:20 +0100

    styling draeing app further to sync in with geenral look and feel

--- js/jquery.js
+++ js/jquery.js
@@ -1,49 +1,9 @@
 $(document).ready(function(){  //start document ready
     
-     verticalText();
-    
-    //CREATE THE IMAGES
-    $('.box-img').each(function(){
-        var t = $(this);
-        createImg(t);
-    }); 
-    
-    
-    //RE-GENERATE IMAGES WHEN WINDOW RESIZED
-    $(window).resize(function() {
-        $('.box-img').each(function(){
-            var t = $(this);
-            createImg(t);
-        }); 
-    });
-    
-    
-
-    
-    //ADD A COLOR BOX OR IMAGE BOX
-    document.oncontextmenu = function() {return false;};
-    
     $('.box').mousedown(function(e){ 
         
         var t = $(this);        
         var h2 = $(this).find('h2');
-            
-        if(h2.length){          
-        }else{
-            var h2t = document.createElement('h2');
-            this.appendChild(h2t);
-            
-            var span = document.createElement('span');
-            span.innerHTML = "This is a description of the box";
-            this.appendChild(span);
-        }
-        
-        //LEFT CLICK ADD IMG
-        if( e.button == 2) { 
-            $(this).addClass('box-img');
-            $(this).find('h2').text("This is an image box");
-            createImg(t);           
-        }
         
         //RIGHT CLICK ADD COLOR
         if( e.button == 0 ) {
@@ -53,54 +13,11 @@ $(document).ready(function(){  //start document ready
            t.css('background-image', '');         
         }
         
-        verticalText(); 
-                
-        
     }); 
     
 
 });// end document ready
     
-
-
-function createImg(t){
-    //get ths size of the box
-    var w = t.width();
-    var h = t.height();
-    
-    //make the image to be the same size of the box
-    var src = "url(http://lorempixel.com/" + w + "/" + h + ")";
-    
-    t.css('background-image', src);     
-}
-
-
-
-function verticalText(){
-    //vertically center the text
-    $('.box').each(function(){
-        
-        //let's vertically center the text
-        var h2 = $(this).find('h2');
-        
-        if(h2.length){
-            //found a <h2> tag
-            var boxHeight = $(this).height()/2;
-            var textHeight = h2.height()/2;
-            var spanHeight = $(this).find('span').height()/2;
-            
-            var height = boxHeight - textHeight - spanHeight - 2; 
-            
-            h2.css('margin-top', height + 'px');
-            
-        }else{
-            //didnt find a <h2>
-        }       
-        
-    });
-        
-}
-
 
 
 function getRandomColor() {
